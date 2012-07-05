1b085fc6b90e392b5c83c24f6740e544fb605d7d
Subrat Rout <subratnrout@gmail.com>
2012-07-05 12:00:59 -0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,7 @@ describe User do
 	it {should respond_to(:name)}
 	it {should respond_to(:email)}
 	it {should respond_to(:password_digest)}
+	it {should respond_to(:password)}
 	it {should respond_to(:password_confirmation)}
 	it {should respond_to(:authenticate)}
 
